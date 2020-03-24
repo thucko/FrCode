d04bfc05f80e6be57646989120edb09da1156467
thucko <timhucko@gmail.com>
2020-03-24 10:09:01 -0700

    Update Fitting_test.py

--- Fitting_test.py
+++ Fitting_test.py
@@ -18,7 +18,7 @@ import random as rd
 
 def chi2(p):
     func = voigt(x_step, p)
-    err = y_e   # Uncertainty in the data sqrt(N)
+    err = y_e # Uncertainty in the data sqrt(N)
     # calculate the chi2
     delta = (y2 - func) / err
     chi2_v = sum(pow(delta, 2))
@@ -28,32 +28,37 @@ def chi2(p):
 def voigt(x, p):
     sigma = p[3]/(2*np.sqrt(2*np.log(2)))
     gamma = p[2]/2
+    #print(sigma)
     z = ((x-p[4]) + 1j*gamma)/(sigma*np.sqrt(2))
     decay = (np.exp(-x / p[5]))
     num = np.real(wofz(z))
     dem = sigma*np.sqrt(2*np.pi)
-    V = p[0]+decay*p[1]*(num/dem)
-    return V
+    v = p[0] + decay*p[1]*(num/dem)
+    return v
 
 
 
-
-x_step = np.arange(-50, 50, 0.1)
+x_step = np.arange(0, 80, 1)
 y =[]
 y2 = []
-
-
+a = 6000
+b_width = 2.8
+n_of_scans = 119.0
+pps = 6.0
 for i in range(len(x_step)):
-    k = rd.gauss(125, 11)
+    k = rd.gauss(a, np.sqrt(a))
+    k = np.sign(k)*k
     #y.append(lorentzian(x[i], [k, 100, 3.5, 0, 50]))
-    y2.append(voigt(x_step[i], [k, 1000, 3.5, 0.6, 0, 350]))
-
+    y2.append(voigt(x_step[i], [k, 6500000.90, 3.877, 1.4899, 40.34, 342]))
+    y2[i] = ((y2[i] + rd.gauss(0, np.sqrt(y2[i]))))
+    if y2[i] < 0:
+        y2[i] = np.sign(y2[i])*y2[i]
 y_e = np.sqrt(y2)
-p1 = [np.min(y2),  np.max(y2)-np.min(y2), 3.5, 0.9, 1, 50]
+p1 = [np.min(y2),  np.max(y2), 3.5, 1,  x_step[np.argmax(np.array(y2))], 350]
 #p1 = [10, 40000, 3.5, 0.6, 0, 350]
 
-m = Minuit.from_array_func(chi2, p1, error=(10, 10, 0.01, 0.001, .1, 1),
-                                   limit=(None, None, None, (0.01, 5), None, None),
+m = Minuit.from_array_func(chi2, p1, error=(0.1, 100, 0.01, 0.01, 1, 10),
+                                   limit=((0, None), (0, None), (0.01, None), (0.01, None), (0, None), (0, None)),
                                    fix=(False, False, False, False, False, False), errordef=1, pedantic=False)
 
 m.migrad()  # This is minimization strategy
@@ -61,7 +66,7 @@ p_fit = [m.values["x0"], m.values["x1"], m.values["x2"], m.values["x3"], m.value
 p_err = [m.errors["x0"], m.errors["x1"], m.errors["x2"], m.errors["x3"], m.errors["x4"], m.errors["x5"]]
 print(m.fval / (len(y2) - len(p_fit)))
 print(p_fit)
-x_fit = np.arange(-50, 50, 0.01)
+x_fit = np.arange(0, 80, 0.01)
 y_fit = voigt(x_fit, p_fit)
 
 residuals = y2 - voigt(x_step, p_fit)
@@ -71,8 +76,8 @@ plt.style.use('ggplot')
 gs = gridspec.GridSpec(2, 2)
 fig = plt.figure(figsize=(16, 9))
 ax1 = fig.add_subplot(gs[0, :])
-ax1.errorbar(x_step, y2, yerr=y_e , fmt='ro', ecolor='black', capsize=5, zorder=1)
-ax1.plot(x_fit, y_fit,  color='#3392FF', zorder=2)
+ax1.errorbar(x_step, np.float64(y2)/(b_width*n_of_scans*pps), yerr=y_e/(b_width*n_of_scans*pps) , fmt='ro', ecolor='black', capsize=5, zorder=1)
+ax1.plot(x_fit, y_fit/(b_width*n_of_scans*pps),  color='#3392FF', zorder=2)
 
 ax2 = fig.add_subplot(gs[1, 0])
 ax2.set_title('Normalized Residuals', fontsize=16)
