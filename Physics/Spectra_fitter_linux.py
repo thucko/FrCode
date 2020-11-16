0da7079f5f4c31e02da3aa9d43c5db8c175a61b1
thucko <timhucko@gmail.com>
2020-11-16 11:06:00 -0800

    no longer support jax

--- Physics/Spectra_fitter_linux.py
+++ Physics/Spectra_fitter_linux.py
@@ -6,13 +6,7 @@ Author: Tim Hucko
 
 '''
 
-import numpy as onp
-from jax.config import config
-config.update("jax_enable_x64", True)
-import jax.numpy as np
-from jax import jit, grad
-from jax.scipy.special import erfc
-from jax.lax import complex
+import numpy as np
 import pandas as pd
 from tkinter import simpledialog
 from tkinter.filedialog import askdirectory
@@ -23,7 +17,7 @@ import glob
 from iminuit import Minuit
 import matplotlib.gridspec as gridspec
 from matplotlib.backends.backend_pdf import PdfPages
-#from scipy.special import wofz
+from scipy.special import wofz
 from tkinter import *
 import matplotlib.pyplot as plt
 import warnings
@@ -36,7 +30,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # Definition for selecting directory
 def dir_dialog():
     Tk().withdraw()
-    pwd = askdirectory()
+    pwd = askdirectory(initialdir='~/Documents')
     return pwd
 
 
@@ -103,10 +97,9 @@ def lorentzian(x, p_lorentzian):
 def voigt(x, p):
         sigma = p[3] / (2 * np.sqrt(2 * np.log(2)))
         gamma = p[2] / 2
-        z = complex(x - p[4], gamma) / (sigma * np.sqrt(2))
-        faddeeva = np.exp(-z ** 2) * erfc(-1j * z)
+        z = (x - p[4] +1j*gamma) / (sigma * np.sqrt(2))
         decay = (np.exp(-x / p[5]))
-        num = np.real(faddeeva)
+        num = np.real(wofz(z))
         dem = sigma * np.sqrt(2 * np.pi)
         V = p[0] + decay * p[1] * (num / dem)
         return V
@@ -121,7 +114,7 @@ def Parameters(data, x, f):
         return p
     elif f == 'Voigt':
 
-         p = (np.min(data), np.max(data), 3.6, 1, 40, 350)
+         p = (np.min(data), np.max(data), 3.6, 1, x[np.argmax(data)], 350)
         # p = [np.min(data), np.max(data), 3.6, 1, device_put(x)[np.argmax(data)], 350]
 
          return p
@@ -194,7 +187,7 @@ class MinimizationRoutine:
             limits = (None, None, (2, 6), None, None)
             fixed = (False, False, False, False, False)
 
-        m = Minuit.from_array_func(jit(chi2), par, error=errors, limit=limits, fix=fixed, errordef=1, pedantic=False)
+        m = Minuit.from_array_func(chi2, par, error=errors, limit=limits, fix=fixed, errordef=1, pedantic=False)
         m.migrad()
 
         if name == 'Voigt':
@@ -283,6 +276,9 @@ mainloop()
 
 # binwidth in ms
 b_width = 2.8
+first_p = 2
+last_p = 8
+pps = last_p - first_p
 
 fit_vals = pd.DataFrame(columns=['Time', 'Offset', 'Peak Height', 'FWHM_L', 'FWHM_G', 'Peak Position', 'Lifetime',
                                      'Reduced Chi Squared'])
@@ -332,10 +328,7 @@ if prompt1 is False:
     for index, row in acq_time.iterrows():
         times = row['Time']
         file_path = row['File path']
-        data_raw = (onp.genfromtxt(file_path))
-        first_p = 2
-        last_p = 8
-        pps = last_p - first_p
+        data_raw = (np.genfromtxt(file_path))
         data_binned = np.array([])
         a = []
 
@@ -452,14 +445,11 @@ elif prompt1 is True:
     # Use file name to get data from file, the files are chronologically ordered
     for index, row in acq_time.iterrows():
         file_path = row['File path']
-        data0.append(onp.genfromtxt(file_path))
+        data0.append(np.genfromtxt(file_path))
     shape = list(data0[0].shape)
     shape[:0] = [len(data0)]
     data1 = np.concatenate(data0).reshape(shape)
     y = data1.sum(axis=0)
-    first_p = 2
-    last_p = 8
-    pps = last_p - first_p
     data_binned = np.array([])
     a = []
     # Gather data points an bin them
@@ -472,9 +462,9 @@ elif prompt1 is True:
     ''' Now to minimize the scans'''
     data = DataManager(sel2)
     data.data_select(data.name, data_binned, x)
-    y_data = np.array(data.y_val)
+    y_data = np.array(data.y_val)/len(data0)
     x_step = np.array(data.x_val)
-    y_err = np.array(data.y_err)
+    y_err = np.array(data.y_err)/len(data0)
     p1 = Parameters(y_data, x_step, sel)  # Generate initial parameter guesses for scans
     val = MinimizationRoutine(sel)
     val.minimzation(val.name, p1)
@@ -525,8 +515,8 @@ elif prompt1 is True:
         'y_fit': y_fit
     })
     #df = pd.melt(repack_data, id_vars=['Freq'], value_vars=['Normalized Residuals', 'Residuals'], var_name='Type')
-    plt.style.use('ggplot')
-    gs = gridspec.GridSpec(2, 2)
+    plt.style.use('cern_root')
+    '''gs = gridspec.GridSpec(2, 2)
     fig = plt.figure(figsize=(16, 9))
 
     ax1 = fig.add_subplot(gs[0, :])
@@ -538,7 +528,7 @@ elif prompt1 is True:
     ax1.set_ylabel('Rate (kHz)', fontsize=16)
     ax1.errorbar(repack_data['Freq'], repack_data['rate'], yerr=repack_data['err'], fmt='ro', ecolor='black',
                  capsize=5, zorder=1)
-    ax1.plot(fit['x_fit'], fit['y_fit'], color='#3392FF', zorder=2)
+    ax1.plot(fit['x_fit'], fit['y_fit'], color='b', zorder=2)
     ax2 = fig.add_subplot(gs[1, 0])
     ax2.set_title('Normalized Residuals', fontsize=16)
     ax2.set_xlabel('Frequency (MHz)', fontsize=14)
@@ -547,8 +537,22 @@ elif prompt1 is True:
     ax3.set_title('Residuals', fontsize=16)
     ax3.set_xlabel('Frequency (MHz)', fontsize=14)
     ax3.plot(repack_data['Freq'], repack_data['Residuals'], '-o', color='#D433FF')
-    fig.subplots_adjust(left=0.06, bottom=0.08, right=0.95, top=0.95, wspace=0.07, hspace=0.25)
-    #plt.show()
+    fig.subplots_adjust(left=0.06, bottom=0.08, right=0.95, top=0.95, wspace=0.07, hspace=0.25)'''
+
+    #quick and dirty plotting
+    plt.errorbar(repack_data['Freq'], repack_data['rate'], yerr=repack_data['err'], fmt='ro', ecolor='black',
+                 capsize=5, zorder=1)
+    plt.plot(fit['x_fit'], fit['y_fit'], color='b', zorder=2)
+    #plt.text(x_fit[0]+20, np.max(y_data)-0.1*np.max(y_data), r'$\chi^2_{reduced}=%.3f$' % Red_chi2, fontsize=14)
+    #plt.text(x_fit[0] + 20, np.max(y_data) - 0.17 * np.max(y_data), r'$\nu_{peak}=%.3f$ MHz' % p_fit[4], fontsize=14)
+    #plt.text(x_fit[0] + 20, np.max(y_data) - 0.24 * np.max(y_data), r'$\delta\nu_{peak}=%.3f$ MHz' % p_err[4],
+    #         fontsize=14)
+    plt.title(r'$^{211}$Fr $\beta$ Transition', size=24)
+    plt.xlabel('Frequency (MHz)', fontsize=16)
+    plt.ylabel('Rate (kHz)', fontsize=16)
+    plt.grid()
+    plt.show()
+
     #plt.savefig('test.pdf')
 
 
